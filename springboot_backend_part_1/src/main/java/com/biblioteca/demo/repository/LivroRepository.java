bc90b677edebed75276339645a5105c0966e36b1
San <sandokam.bussola@alunos.sc.senac.br>
2024-09-04 20:43:59 -0300

    fix: Apagando branch

--- springboot_backend_part_1/src/main/java/com/biblioteca/demo/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.demo.repository;
-
-import com.biblioteca.demo.entity.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
--- springboot_backend_part_1/src/main/java/com/biblioteca/demo/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.demo.repository;
-
-import com.biblioteca.demo.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
