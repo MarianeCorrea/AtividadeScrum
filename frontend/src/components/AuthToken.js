5ab897bbedea8f78cb5f6139ca600ea2e41f548e
Leonardo Cabral <111531879+leleku10@users.noreply.github.com>
2024-09-14 13:21:10 -0300

    Update AuthToken.js

--- frontend/src/components/AuthToken.js
+++ frontend/src/components/AuthToken.js
@@ -1,50 +1,27 @@
-<<<<<<< HEAD
 export const loginUser = async (email, senha) => {
-=======
-
-
-export const loginUser = async (username, password) => {
->>>>>>> 40b417e3c4a2c5d5d4a805331d40234e267a202a
   try {
     const response = await fetch('http://localhost:8080/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-<<<<<<< HEAD
-        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({ email, senha }),
     });
 
-    
-
-=======
-      },
-      body: JSON.stringify({ username, password }),
-    });
-
->>>>>>> 40b417e3c4a2c5d5d4a805331d40234e267a202a
     if (!response.ok) {
-      throw new Error('Falha na autenticação');
+      const errorText = await response.text(); // Obter texto do erro, se possível
+      throw new Error(`Falha na autenticação: ${errorText}`);
     }
 
     const data = await response.json();
-<<<<<<< HEAD
     
     // Adicione um console.log para verificar a resposta do backend
     console.log('Resposta do backend:', data);
 
     // Supondo que o token está na propriedade `token` do objeto retornado
-    return data.token; 
-  } catch (error) {
-    console.error('Erro ao realizar login teste de console:', error);
-=======
-
-    // Supondo que o token está na propriedade `token` do objeto retornado
     return data.token; // Ajuste conforme necessário
   } catch (error) {
     console.error('Erro ao realizar login:', error);
->>>>>>> 40b417e3c4a2c5d5d4a805331d40234e267a202a
     throw error;
   }
 };
